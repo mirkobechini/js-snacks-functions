aebe002f6dcd24a76729a0eef532e2390b04ff9d
mirkobechini <mirkobechini@gmail.com>
2025-07-17 21:42:44 +0200

    ex 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,52 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowelsNum(word){
+    let occurances = 0
+    for(let i = 0; i < word.length; i++){
+        const letter = word[i]
+        if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+            occurances++
+        }
+    }
+    return occurances
+
+}
+
+/*
+const vowelsNum = (word) => {
+    let occurances = 0
+    for(let i = 0; i < word.length; i++){
+        const letter = word[i]
+        if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+            occurances++
+        }
+    }
+    return occurances
+
+}
+
+const vowelsNum = word => {
+    let occurances = 0
+    for(let i = 0; i < word.length; i++){
+        const letter = word[i]
+        if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+            occurances++
+        }
+    }
+    return occurances
+
+}
+
+
+
+*/
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vowelsNum(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
