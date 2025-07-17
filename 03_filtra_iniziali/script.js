a10bf4da06be331636031b89c6414e9140242600
mirkobechini <mirkobechini@gmail.com>
2025-07-17 21:37:00 +0200

    ex 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,34 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function nameFromLetter(array, letter){
+    const arrayFiltered = []
+    for(let i = 0; i < array.length; i++){
+        const thisItem = array[i]
+        if(thisItem[0] === letter){
+            arrayFiltered.push(thisItem)
+        }
+    }
+    return arrayFiltered
+}
 
+/*
+
+const nameFromLetter = (array, letter) => {
+    const arrayFiltered = []
+    for(let i = 0; i < array.length; i++){
+        const thisItem = array[i]
+        if(thisItem[0] === letter){
+            arrayFiltered.push(thisItem)
+        }
+    }
+    return arrayFiltered
+}
+
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(nameFromLetter(names, "A"))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
